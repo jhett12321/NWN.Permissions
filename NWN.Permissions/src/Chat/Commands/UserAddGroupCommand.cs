9dfd9f989f885729818b7673e97cb0096e2fd1f1
Jorteck <jorteckdev@gmail.com>
2021-10-22 22:53:03 +0200

    Disable add user group command for now.

--- NWN.Permissions/src/Chat/Commands/UserAddGroupCommand.cs
+++ NWN.Permissions/src/Chat/Commands/UserAddGroupCommand.cs
@@ -1,38 +1,38 @@
-using System.Collections.Generic;
-using Anvil.API;
-using Anvil.Services;
-
-namespace Jorteck.Permissions
-{
-  internal class UserAddGroupCommand : ICommand
-  {
-    private readonly PermissionsConfigService permissionsConfigService;
-    private readonly CursorTargetService cursorTargetService;
-
-    public string Command { get; }
-    public string Permission { get; }
-    public int? ArgCount { get; }
-    public string Description { get; }
-    public string[] Usages { get; }
-
-    public UserAddGroupCommand(PermissionsConfigService permissionsConfigService, CursorTargetService cursorTargetService)
-    {
-      this.permissionsConfigService = permissionsConfigService;
-      this.cursorTargetService = cursorTargetService;
-
-      Command = "group adduser";
-      Permission = PermissionConstants.UserAddGroup;
-      ArgCount = null;
-      Description = "Adds a group membership to a user.";
-      Usages = new[]
-      {
-        $"{Command} <group_name>    You will be prompted to select the player with your cursor.",
-      };
-    }
-
-    public void ProcessCommand(NwPlayer caller, IReadOnlyList<string> args)
-    {
-      string group = args[0];
-    }
-  }
-}
+// using System.Collections.Generic;
+// using Anvil.API;
+// using Anvil.Services;
+//
+// namespace Jorteck.Permissions
+// {
+//   internal class UserAddGroupCommand : ICommand
+//   {
+//     private readonly PermissionsConfigService permissionsConfigService;
+//     private readonly CursorTargetService cursorTargetService;
+//
+//     public string Command { get; }
+//     public string Permission { get; }
+//     public int? ArgCount { get; }
+//     public string Description { get; }
+//     public string[] Usages { get; }
+//
+//     public UserAddGroupCommand(PermissionsConfigService permissionsConfigService, CursorTargetService cursorTargetService)
+//     {
+//       this.permissionsConfigService = permissionsConfigService;
+//       this.cursorTargetService = cursorTargetService;
+//
+//       Command = "group adduser";
+//       Permission = PermissionConstants.UserAddGroup;
+//       ArgCount = null;
+//       Description = "Adds a group membership to a user.";
+//       Usages = new[]
+//       {
+//         $"{Command} <group_name>    You will be prompted to select the player with your cursor.",
+//       };
+//     }
+//
+//     public void ProcessCommand(NwPlayer caller, IReadOnlyList<string> args)
+//     {
+//       string group = args[0];
+//     }
+//   }
+// }
