8830c59fc384522a417f31a712beb9beee30c353
Jakob Knutsen <jakobknutsen@gmail.com>
2021-12-31 09:18:22 +0100

    Reuse event validation through extension

--- NWN.Permissions/src/ChatCommands/UserListPermsCommand.cs
+++ NWN.Permissions/src/ChatCommands/UserListPermsCommand.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using Anvil.API;
-using Anvil.API.Events;
 using Anvil.Services;
 using Jorteck.ChatTools;
 
@@ -31,34 +30,14 @@ namespace Jorteck.Permissions
 
     public void ProcessCommand(NwPlayer caller, IReadOnlyList<string> args)
     {
-      caller.EnterTargetMode(eventData => ProcessEventOnValidPCTargetBy(eventData, caller, ListPermissionsOfTargetToCaller));
-    }
-
-    private void ProcessEventOnValidPCTargetBy(
-      ModuleEvents.OnPlayerTarget eventData,
-      NwPlayer caller,
-      Action<NwPlayer, NwPlayer> callback)
-    {
-      if (eventData.IsCancelled)
-      {
-        return;
-      }
-
-      if (!(eventData.TargetObject is NwCreature targetCreature &&
-            targetCreature.IsLoginPlayerCharacter(out NwPlayer targetPlayer)))
-      {
-        eventData.Player.SendErrorMessage("Target must be a player character.");
-        return;
-      }
-
-      callback(targetPlayer, caller);
+      caller.EnterTargetMode(eventData => eventData.ProcessOnValidPCTargetBy(caller, ListPermissionsOfTargetToCaller));
     }
 
     private void ListPermissionsOfTargetToCaller(NwPlayer target, NwPlayer caller)
     {
       PermissionSet userPermissions = ConfigService.GetPermissionsForPlayer(target);
-      caller.SendServerMessage($"{target.CDKey} ({target.LoginCreature.Name}) has {(userPermissions.Permissions.Count == 0 ? "no permissions." : "the following permissions:")}", ColorConstants.White);
-      caller.SendServerMessage(String.Join("\n", userPermissions.Permissions), ColorConstants.White);
+      caller.SendServerMessage($"Target has {(userPermissions.Permissions.Count == 0 ? "no permissions." : "the following permissions:")}", ColorConstants.Orange);
+      caller.SendServerMessage(String.Join("\n", userPermissions.Permissions), ColorConstants.Lime);
     }
   }
 }
